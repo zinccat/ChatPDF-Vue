ee78dc4242cabddf2188de264486981917165d5b
ZincCat <zincchloride@outlook.com>
2024-02-01 21:49:29 -0500

    refactor

--- chatpdf/src/components/SearchComponent.vue
+++ chatpdf/src/components/SearchComponent.vue
@@ -9,47 +9,6 @@
 </template>
   
 <script setup>
-import { ref } from 'vue'
-import axios from 'axios'
-import { highlightRows } from './utils.js'
-import { pdfViewer, performRequestWithExponentialBackoff, combineTextAndMap } from './utils.js'
-
-const searchQuery = ref("")
-
-// Search text within the PDF and interact with a backend service
-const performSearch = async () => {
-    if (!searchQuery.value || !pdfViewer.value) {
-        console.error('Search query or PDF viewer not available')
-        return
-    }
-    const textLayers = pdfViewer.value.querySelectorAll('.textLayer')
-    const { combinedText, textMappings } = combineTextAndMap(textLayers)
-
-    // eslint-disable-next-line no-unused-vars
-    const sendRequest = async () => {
-        return axios.post('http://localhost:5005/process_text', { text: combinedText, query: searchQuery.value })
-    }
-    
-    try {
-        console.log('sendRequest')
-        const response = await performRequestWithExponentialBackoff(sendRequest)
-        console.log(response.data.highlight)
-        const paragraphPosition = findParagraphPosition(combinedText, response.data.highlight)
-        console.log(paragraphPosition)
-        if (paragraphPosition.startPosition === -1) {
-            console.error('Highlight not found in combined text')
-            return
-        }
-        highlightRows(textMappings, paragraphPosition)
-    } catch (error) {
-        console.error('Error:', error)
-    }
-}
-
-// Process highlight response from backend
-function findParagraphPosition(combinedText, paragraph) {
-    const startPosition = combinedText.indexOf(paragraph)
-    return { startPosition, endPosition: startPosition + paragraph.length - 1 }
-}
+import { performSearch, searchQuery  } from './utils.js'
 </script>
   
--- chatpdf/src/components/utils.js
+++ chatpdf/src/components/utils.js
@@ -1,4 +1,5 @@
 import { ref } from "vue";
+import axios from "axios";
 
 export function highlightRows(textMappings, paragraphPosition) {
   textMappings.value
@@ -54,6 +55,43 @@ const getNodeText = (node) => node.nodeType === Node.TEXT_NODE ? node.nodeValue 
   node.nodeType === Node.ELEMENT_NODE ? Array.from(node.childNodes).map(getNodeText).join(' ') : ''
 
 
+// Search text within the PDF and interact with a backend service
+export const performSearch = async () => {
+  if (!searchQuery.value || !pdfViewer.value) {
+      console.error('Search query or PDF viewer not available')
+      return
+  }
+  const textLayers = pdfViewer.value.querySelectorAll('.textLayer')
+  const { combinedText, textMappings } = combineTextAndMap(textLayers)
+
+  // eslint-disable-next-line no-unused-vars
+  const sendRequest = async () => {
+      return axios.post('http://localhost:5005/process_text', { text: combinedText, query: searchQuery.value })
+  }
+  
+  try {
+      console.log('sendRequest')
+      const response = await performRequestWithExponentialBackoff(sendRequest)
+      console.log(response.data.highlight)
+      const paragraphPosition = findParagraphPosition(combinedText, response.data.highlight)
+      console.log(paragraphPosition)
+      if (paragraphPosition.startPosition === -1) {
+          console.error('Highlight not found in combined text')
+          return
+      }
+      highlightRows(textMappings, paragraphPosition)
+  } catch (error) {
+      console.error('Error:', error)
+  }
+}
+
+// Process highlight response from backend
+function findParagraphPosition(combinedText, paragraph) {
+  const startPosition = combinedText.indexOf(paragraph)
+  return { startPosition, endPosition: startPosition + paragraph.length - 1 }
+}
+
 export const pdfViewer = ref(null);
 export const pdfRenderKey = ref(0);
 export const textMappings = ref([]);
+export const searchQuery = ref("")
